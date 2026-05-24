cecf0f876ee5eb3da6320ae6186be991ce747b35
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-05-24 21:58:31 +0530

    Added new features and updated project

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,37 +1,69 @@
-import React  from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { motion } from 'motion/react'
-import { BsRobot,BsCoin } from 'react-icons/bs'
+import { BsRobot, BsCoin } from 'react-icons/bs'
 import { HiOutlineLogout } from 'react-icons/hi'
 import { FaUserAstronaut } from 'react-icons/fa'
+
 function Navbar() {
-    const {userData} = useSelector((state)=>state.user)
-  return (
-    <div className='bg-[#F3F3F3] flex justify-center px-4 pt-6'>
-        <motion.div 
-        initial={{opacity:0,y:-40}} 
-        animate={{opacity:1,y:0}}
-        transition={{duration:0.3}}
-        className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm  border 
+    const { userData } = useSelector((state) => state.user)
+    const [showCreditPopup,setShowCreditPopup]=useState(false) 
+    const [showUserPopup,setShowUserPopup]=useState(false)
+    const navigate = UseNavigate()
+    return (
+        <div className='bg-[#F3F3F3] flex justify-center px-4 pt-6'>
+            <motion.div
+                initial={{ opacity: 0, y: -40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+                className='w-full max-w-6xl bg-white rounded-[24px] shadow-sm  border 
         border-gray-200  px-8 py-4 flex justify-between items-center relative'>
-            <div className='flex items-center gap-3 cursor-pointer'>
-                <div className='bg-black text-white p-2 rounded-lg'>
-                    <BsRobot size={18}/>
+                <div className='flex items-center gap-3 cursor-pointer'>
+                    <div className='bg-black text-white p-2 rounded-lg'>
+                        <BsRobot size={18} />
+                    </div>
+
+                    <h1 className='font-semibold hidden md:block text-lg'>InterviewIQ.AI</h1>
                 </div>
-                <h1 className='font-semibold hidden md:block text-lg'>InterviewIQ.AI</h1>
+                 
                 <div className='flex items-center gap-6 relative'>
                     <div className='relative'>
-                        <button className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
-                            <BsCoin size={20}/>
+                        <button onClick={()=>setShowCreditPopup(!showCreditPopup)} 
+                        className='flex items-center gap-2 bg-gray-100 px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
+                            <BsCoin size={20} />
                             {userData?.credits || 0}
                         </button>
+                        {showCreditPopup && (
+                        <div className='absolute rigth-[-50] mt-3 w-64 bg-white shadow-xl border border-gray-200 rounded p-5 z-50'>
+                             <p className='text-sm text-gray-600 mb-4'>Need more credits to continue interview?</p>
+                             <button onClick={()=>navigate("/pricing")} 
+                             xclassName='w-full bg-black text-white py-2 rounded-lg text-sm'>But more credits</button>
+                        </div>
+                        )}
                     </div>
-                    <div></div>
+
+                    <div className='relative'>
+                        <button onClick={()=>setShowUserPopup(!showUserPopup)} 
+                        className='w-9 h-9 bg-black text-white rounded-full flex items-center justify-center font-semibold'>
+                            {userData ? userData?.name.slice(0,1).toUpperCase()
+                            :<FaUserAstronaut size={16}/>}
+                        </button>
+
+                        {showUserPopup && (
+                            <div className='absolute ring-0 mt-3 w-48 bg-white shadow-xl border border-gray-200 rounded-xl p-4 z-50'>
+                                <p className='text-md text-blue-500 font-medium mb-1'>{userData?.name}</p>
+                                <button onClick={()=> navigate("/history")} className='w-full text-left text-sm py-2 hover:text-black text-gray-600 '>Interview History</button>
+                                <button className='w-full text-left text-sm py-2 flex items-center gap-2'>
+                                    <HiOutlineLogout size={16}/>
+                                    Logout</button>
+                            </div>
+                        )}
+                    </div>
+
                 </div>
-            </div>
-        </motion.div>
-    </div>
-  )
+            </motion.div>
+        </div>
+    )
 }
 
 export default Navbar
