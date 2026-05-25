7a11b3adc4fe77aa77a88d3342f2f4dc86d13b90
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-05-25 23:37:52 +0530

    Added new features and updated project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,15 @@ import { BsRobot, BsMic, BsClock, BsBarChart, BsFileEarmarkText } from 'react-ic
 import { HiSparkles } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
 import AuthModel from '../components/AuthModel';
+import hrImg from "../assets/HR.png"
+import techImg from "../assets/tech.png"
+import confidenceImg from "../assets/confi.png"
+import evalImg from "../assets/ai-ans.png"
+import resumeImg from "../assets/resume.png"
+import pdfImg from "../assets/pdf.png"
+import analyticsImg from "../assets/history.png"
+
+
 
 function Home() {
   const { userData } = useSelector((state) => state.user)
@@ -16,6 +25,8 @@ function Home() {
       <Navbar />
 
       <div className='flex-1 px-6 py-20'>
+        <div className='max-w-6xl mx-auto'>
+
         <div className='flex justify-center mb-6'>
           <div className='bg-gray-100 text-gray-700 text-sm px-4 py-2 rounded-full flex items-center gap-2'>
             <HiSparkles size={16} className="bg-green-50 text-green-600" />
@@ -120,23 +131,134 @@ function Home() {
                   <p className='text-sm text-gray-500 leading-relaxed'>{item.desc}</p>
                 </div>
 
-
               </motion.div>
             ))
           }
         </div>
 
-          <div className='mb-32'>
-          <motion.h2 
-           initial={{ opacity: 0, y: 20 }}
-           whileInView={{ opacity: 1, y: 0 }}
-           transition={{ duration: 0.6}}
-           className='text-4xl font-semibold text-center mb-16'>
+        <div className='mb-32'>
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            className='text-4xl font-semibold text-center mb-16'>
             Advance AI{" "}
-          <span className='text-green-600'>Capabilities</span>
+            <span className='text-green-600'>Capabilities</span>
           </motion.h2>
-          </div>
 
+          <div className='grid md:grid-cols-2 gap-10'>
+            {
+              [
+                {
+                  image: evalImg,
+                  icon: <BsBarChart size={20} />,
+                  title: "AI Answer Evaluation",
+                  desc: "Scores communication, technical accuracy and confidence."
+                },
+                {
+                  image: resumeImg,
+                  icon: <BsFileEarmarkText size={20} />,
+                  title: "Resume Based Interview",
+                  desc: "Project-Specific questionsbased on uploaded resume."
+                },
+                {
+                  image: pdfImg,
+                  icon: <BsFileEarmarkText size={20} />,
+                  title: "Download PDF Report",
+                  desc: "Detailed strengths, weaknesses and improvement insights."
+                },
+                {
+                  image: analyticsImg,
+                  icon: <BsBarChart size={20} />,
+                  title: "History & Analytics",
+                  desc: "Track progress with perfomance graphs and topic analysis."
+                }
+              ].map((item, index) => (
+                <motion.div key={index}
+                  initial={{ opacity: 0, y: 30 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 + index * 0.1 }}
+                  whileHover={{ scale: 1.02 }}
+                  className='bg-white border border-gray-200 rounded-3xl 
+                p-8 shadow-sm hover:shadow-xl transition-all'>
+                  <div className='flex flex-col md:flex-row items-center gap-8'>
+                    <div className='w-full md:w-1/2 flex justify-center'>
+                      <img src={item.image} alt={item.title} className='w-full h-auto object-contain max-h-64' />
+                    </div>
+
+                    <div className='w-full md:w-1/2'>
+                      <div className='bg-green-50 text-green-600 w-12 h-12 rounded-xl flex items-center justify-center mb-6'>
+                        {item.icon}
+                      </div>
+                      <h3 className='font-semibold mb-3 text-xl'>{item.title}</h3>
+                      <p className='text-gray-500 text-sm leading-relaxed'>{item.desc}</p>
+                    </div>
+                  </div>
+                </motion.div>
+              ))
+            }
+          </div>
+        </div>
+
+        <div className='mb-32'>
+          <motion.h2
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            className='text-4xl font-semibold text-center mb-16'>
+            Multiple Interview{" "}
+            <span className='text-green-600'>Modes</span>
+          </motion.h2>
+
+          <div className='grid md:grid-cols-2 gap-10'>
+            {
+              [
+                {
+                  image: hrImg,
+                  title: "HR Interview Mode",
+                  desc: "Behavioral and commiunication based evaluation."
+                },
+                {
+                  image: techImg,
+                  title: "Technical Mode",
+                  desc: "Deep technical questioning based on selected role."
+                },
+                {
+                  image: confidenceImg,
+                  title: "Confidence Detection",
+                  desc: "Basic tone and voice analysi insights."
+                },
+                {
+                  image: confidenceImg,
+                  title: "Credits System",
+                  desc: "Unlock premium interview sessions easily."
+                }
+              ].map((mode, index) => (
+                <motion.div key={index}
+                  initial={{ opacity: 0, y: 30 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 , delay:index * 0.1 }}
+                  whileHover={{ y:-6 }}
+                  className='bg-white border border-gray-200 rounded-3xl p-8 shadow-sm hover:shadow-xl transition-all'>
+                  <div className='flex items-center justify-between gap-6'>        
+                    <div className='w-1/2'>
+                    <h3 className=' font-semibold text-xl mb-3'>
+                      {mode.title}
+                    </h3>
+                    <p className=' text-gray-500 text-sm leading-relaxed '>{mode.desc}</p>
+                    </div>    
+
+                    <div className='w-1/2 flex justify-end'>
+                      <img src={mode.img} alt={mode.title} className="w-28 h-28 object-contain"/>
+                    </div>
+                   </div>
+                </motion.div>
+              ))
+            }
+          </div>
+        </div>
+
+      </div>
       </div>
 
       {showAuth && <AuthModel onClose={() => setShowAuth(false)} />}
