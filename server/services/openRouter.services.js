c508d2ba18fa3d72020bf08d56097daf854cf863
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-05-29 14:14:33 +0530

    Added new features and updated project

--- server/services/openRouter.services.js
+++ server/services/openRouter.services.js
@@ -31,38 +31,27 @@
 import axios from "axios"
 export const askAi = async (messages) => {
     try {
-
-        if (
-            !messages || !Array.isArray(messages) || messages.length === 0
-        ) {
-            throw new Error(
-                "messages array is empty"
-            )
+        if (!messages || !Array.isArray(messages) || messages.length === 0) {
+            throw new Error("messages array is empty")
         }
-
         const response =await axios.post("https://openrouter.ai/api/v1/chat/completions",
                 {
                     model:"openai/gpt-4o-mini",
-                    messages
+                    messages:messages
                 },
                 {
                     headers: {
                         Authorization:`Bearer ${process.env.OPENROUTER_API_KEY}`,
                         "Content-Type":"application/json"
-                    }
-                }
-            )
+                    }})
         console.log(response.data)
 
         const content =response?.data?.choices?.[0]?.message ?.content
-        if (
-            !content ||!content.trim()
-        ) {
+        if (!content ||!content.trim()) {
             throw new Error("AI returned empty response")
         }
         return content
-    }
-    catch (error) {
+    }catch (error) {
         console.error("OpenRouter Error:",
             error.response?.data|| error.message
         )
