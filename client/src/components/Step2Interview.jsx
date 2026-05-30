a0cb0f1cf3a096086efb97bb2136f9417797647b
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-05-30 13:04:12 +0530

    Added new features and updated project

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,32 +1,113 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import malevideo from "../assets/video/male-ai.mp4"
 import femalevideo from "../assets/video/female-ai.mp4"
 import Timer from './Timer'
-import { motion, scale } from "motion/react"
-import { FaMicrophone, FaMicrophoneSlash} from "react-icons/fa"
+import { motion } from "motion/react"
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa"
 
-function Step2Interview(interviewData, onFinish) {
-const { interviewId, questions, userName } = interviewData;
-const [isIntroPhase, setIsIntroPhase] = useState(true);
+function Step2Interview({ interviewData, onFinish }) {
+  const { interviewId, questions = [], userName } = interviewData || {};
+  const [isIntroPhase, setIsIntroPhase] = useState(true);
 
-const [isMicOn, setIsMicOn] = useState(true);
-const recognitionRef = useRef(null);
-const [isAIPlaying, setIsAIPlaying] = useState(false);
+  const [isMicOn, setIsMicOn] = useState(true);
+  const recognitionRef = useRef(null);
+  const [isAIPlaying, setIsAIPlaying] = useState(false);
 
-const [currentIndex, setCurrentIndex] = useState(0);  
-const [answer, setAnswer] = useState("");
-const [feedback, setFeedback] = useState("");
-const [timeLeft, setTimeLeft] = useState(
-  questions?.[0]?.timelimit || 60 
-);  
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [answer, setAnswer] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [timeLeft, setTimeLeft] = useState(
+    questions[0]?.timelimit || 60
+  );
 
-const [selectedVoice, setSelectedVoice] = useState(null);
-const [isSubmitting, setIsSubmitting] = useState(false);
-const [voiceGender, setVoiceGender] = useState("female");
-const [subtitle, setSubtitle] = useState("");
-const videoRef = useRef(null);
+  const [selectedVoice, setSelectedVoice] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [voiceGender, setVoiceGender] = useState("female");
+  const [subtitle, setSubtitle] = useState("");
 
-const currentQuestion = questions[currentIndex];
+  const videoRef = useRef(null);
+
+  const currentQuestion = questions[currentIndex];
+
+  useEffect(() => {
+    const loadVoices = () => {
+      const voices = window.speechSynthesis.getVoices();
+      if (!voices.length) return;
+
+      const femaleVoice = voices.find(v =>
+        v.name.toLowerCase().includes("zira") ||
+        v.name.toLowerCase().includes("samantha") ||
+        v.name.toLowerCase().includes("female")
+      );
+
+      if (femaleVoice) {
+        setSelectedVoice(femaleVoice);
+        setVoiceGender("female");
+        return;
+      }
+
+      const maleVoice = voices.find(v =>
+        v.name.toLowerCase().includes("david") ||
+        v.name.toLowerCase().includes("mark") ||
+        v.name.toLowerCase().includes("male")
+      );
+      if (maleVoice) {
+        setSelectedVoice(maleVoice);
+        setVoiceGender("male");
+        return;
+      }
+      setSelectedVoice(voices[0]);
+      setVoiceGender("female");
+    };
+    loadVoices();
+    window.speechSynthesis.onvoiceschanged = loadVoices;
+  }, [])
+
+  const videoSource = voiceGender === "male" ? malevideo : femalevideo;
+
+  const speakText = (text) => {
+    return new Promise((resolve) => {
+      if (!window.speechSynthesis || !selectedVoice) {
+        resolve();
+        return;
+      }
+
+      window.speechSynthesis.cancel();
+
+      const humanText = text
+      .replace(/,/g,", ...")
+      .replace(/\./g,". ...");
+
+      const utterance = new SpeechSynthesisUtterance(humanText);
+
+      utterance.voice = selectedVoice;
+
+      utterance.rate = 0.92;
+      utterance.pitch = 1.05;
+      utterance.volume = 1;
+
+      utterance.onstart = () => {
+        setIsAIPlaying(true);
+        videoRef.current?.play();
+      };
+
+      utterance.onend = () => {
+        videoRef.current?.pause();
+        videoRef.current.currentTime = 0;
+        setIsAIPlaying(false);
+      }
+
+      setTimeout(() => {
+        setSubtitle("");
+        resolve();
+      }, 300);
+
+    setSubtitle(text);
+    
+    window.speechSynthesis.speak(utterance);
+  });
+};
+
   return (
     <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white 
     to-teal-100 flex items-center justify-center p-4 sm:p-6'>
@@ -35,11 +116,24 @@ const currentQuestion = questions[currentIndex];
 
         {/* video section */}
         <div className='w-full lg:w-[35%] bg-white flex flex-col items-center 
-        p-6 space-y-6 border-rborder-gray-200'>
+        p-6 space-y-6 border-r border-gray-200'>
           <div>
-            <video src={femalevideo} muted playsInline preload="auto"
-              className='w-full max-w-md rounded-2xl outline-hidden shadow-xl' />
+            <video
+              src={videoSource}
+              key={videoSource}
+              ref={videoRef}
+              muted
+              playsInline
+              preload="auto"
+              className='w-full h-auto  object-cover' />
           </div>
+
+          {/* subtitle pending */}
+          {subtitle &&(
+            <div className='w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl shadow-sm'>
+              <p className='text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed'>{subtitle}</p>
+            </div>
+          )}
 
           <div className='w-full max-w-md bg-white border border-gray-200 
           rounded-2xl shadow-md p-6 space-y-5'>
@@ -47,9 +141,9 @@ const currentQuestion = questions[currentIndex];
               <span className='text-green-500 text-sm'>
                 Interview Status
               </span>
-              <span className='text-sm font-semibold text-emerald-600'>
-                AI Speaking
-              </span>
+              {isAIPlaying && <span className='text-sm font-semibold text-emerald-600'>
+                {isAIPlaying ? "AI Speaking" : ""}
+              </span>}
             </div>
             <div className='h-px bg-gray-200'></div>
 
@@ -59,11 +153,12 @@ const currentQuestion = questions[currentIndex];
             <div className='h-px bg-gray-200'></div>
             <div className='grid grid-cols-2 gap-6 text-center'>
               <div>
-                <span className='text-2xl font-bold text-emerald-600'>1</span>
+                <span className='text-2xl font-bold text-emerald-600'>
+                  {currentIndex + 1}</span>
                 <span className='text-sm text-gray-400'>Current Questions</span>
               </div>
               <div>
-                <span className='text-2xl font-bold text-emerald-600'>5</span>
+                <span className='text-2xl font-bold text-emerald-600'>{questions.length}</span>
                 <span className='text-sm text-gray-400'>Total Questions</span>
               </div>
             </div>
@@ -76,9 +171,10 @@ const currentQuestion = questions[currentIndex];
           <h2 className='text-xl sm:text-2xl font-bold text-emerald-600 mb-6'>AI smart interview</h2>
           <div className='relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-lg border border-gray-200 shadow-sm'>
             <p className=' text-sm sm:text-sm text-gray-400 mb-2'>
-              Question 1 of 5
+              Question {currentIndex + 1} of {questions.length}
             </p>
-            <div className='text-base sm:text-lg font-semibold text-gray-800 leading-relaxed'>{currentQuestion?.question}</div>
+            <div className='text-base sm:text-lg font-semibold text-gray-800 leading-relaxed'>
+              {currentQuestion?.question}</div>
           </div>
 
           <textarea
@@ -88,19 +184,19 @@ const currentQuestion = questions[currentIndex];
           focus:ring-emerald-500 transition text-gray-800'/>
 
           <div className='flex items-center gap-4 mt-6'>
-            <motion.button 
-          whileTap={{scale:0.9}}
-            className='w-12 h-12 sm:w-14 sm:h-14 flex items-center 
-            justify-center rounded-full bg-black text-white shadow-lg'> 
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              className='w-12 h-12 sm:w-14 sm:h-14 flex items-center 
+            justify-center rounded-full bg-black text-white shadow-lg'>
               <FaMicrophone size={20} />
-            </motion.button>  
+            </motion.button>
 
-            <motion.button 
-            whileTap={{scale:0.95}}
-            className='flex-1 bg-gradient-to-r from-emerald-600 to-teal-500
+            <motion.button
+              whileTap={{ scale: 0.95 }}
+              className='flex-1 bg-gradient-to-r from-emerald-600 to-teal-500
              text-white py-3 sm:py-4 rounded-2xl shadow-lg hover:opacity-90 transition font-semibold'>
               Submit Answer
-             </motion.button>
+            </motion.button>
           </div>
         </div>
       </div>
