9fef4b3acf0f3f334ba29c13f12dd925a87c5f12
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-06-02 09:34:36 +0530

    Added new features and updated project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
         <Route path='/interview' element={<Interviewpage/>}/>
         <Route path='/history' element={<InterviewHistory/>}/>
         <Route path='/pricing' element={<Pricing/>}/>
-        <Route path='/report' element={<InterviewReport/>}/>
+        <Route path='/report/:id' element={<InterviewReport/>}/>
     </Routes>
   )
 }
--- client/src/pages/InterviewReport.jsx
+++ client/src/pages/InterviewReport.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function InterviewReport() {
   return (
-    <div>InterviewReport</div>
+    <div>Report</div>
   )
 }
 
