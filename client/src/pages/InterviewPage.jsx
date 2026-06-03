01e5e31cf3326ac1e1b7c40b77d7a699d85dc47a
Hirapara Dishant Sanjaybhai <159747933+diiiishant@users.noreply.github.com>
2026-06-03 18:13:34 +0530

    Update InterviewPage.jsx

--- client/src/pages/InterviewPage.jsx
+++ client/src/pages/InterviewPage.jsx
@@ -3,7 +3,7 @@ import Step1SetUp from '../components/Step1SetUp'
 import Step2Interview from '../components/Step2Interview'
 import Step3Report from '../components/Step3Report'
  
- function Interviewpage() {
+ function InterviewPage() {
     const [step,setStep] = useState(1)
     const [interviewData,setInterviewData] = useState(null)
    return (
