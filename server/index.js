d7fcf71c3d1dd75129c502be04c3ed3c689ed0f0
Dishant Hirapara <hiraparadishant123@gmail.com>
2026-06-05 15:02:44 +0530

    Fix CORS dynamic origin

--- server/index.js
+++ server/index.js
@@ -36,18 +36,18 @@ app.use("/api/user",userRouter)
 app.use("/api/interview",interviewRouter)
 app.use("/api/payment",paymentRouter)
 
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
-    app.get("/:splat*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../client/dist")));
+//     app.get("/:splat*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+//     });
+// }
 
 const PORT = process.env.PORT || 6000
 
